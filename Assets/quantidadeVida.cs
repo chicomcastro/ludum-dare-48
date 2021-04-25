0f2b1bc740419986395ea26433314890c59b4e8f
hirojinho <eduardo.haraguchi@gmail.com>
2021-04-25 17:22:15 -0300

    coracao

--- Assets/quantidadeVida.cs
+++ Assets/quantidadeVida.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class quantidadeVida : MonoBehaviour
 {
-    int threshold = 20;
+    public int threshold = 20;
     public GameObject player;
     public Sprite half;
+    public int multiplicador = 1;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +16,24 @@ public class quantidadeVida : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        if(player.GetComponent<sis_health>().health <= threshold)
-        {
-            this.gameObject.GetComponent<SpriteRenderer>().sprite = half;
-        }
+    {   
+            if(player.GetComponent<sis_health>().health < threshold)
+            {
+                this.gameObject.GetComponent<SpriteRenderer>().sprite = half;
+            }
 
-        if(player.GetComponent<sis_health>().health <= threshold - 10)
-        {
-            Destroy(gameObject);
-        }
+            if(player.GetComponent<sis_health>().health <= threshold - 10)
+            {
+                Destroy(gameObject);
+                multiplicador--;
+            }
+
+            if(player.GetComponent<sis_health>().health > threshold && multiplicador == 1)
+            {
+                threshold += 20;
+                Instantiate(gameObject, new Vector3(gameObject.GetComponent<Transform>().position.x + 1, gameObject.GetComponent<Transform>().position.y, gameObject.GetComponent<Transform>().position.z), Quaternion.identity); 
+                threshold -= 20;
+                multiplicador++;
+            }
     }
 }
