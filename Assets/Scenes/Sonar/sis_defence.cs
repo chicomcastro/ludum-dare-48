b93e43f4bdfe21f2485a12428873b0481413993a
Rafael450 <rafaelgamer450@gmail.com>
2021-04-25 00:45:23 -0300

    arrumei a escada, só falta arrumar o bgl da horizontal

--- Assets/Scenes/Sonar/sis_defence.cs
+++ Assets/Scenes/Sonar/sis_defence.cs
@@ -7,14 +7,10 @@ public class sis_defence : MonoBehaviour
     
     float defense; // a ideia aqui eh que se um jogador com defesa x, se um inimigo atacar ele com um ataque da dano y, o dano passa a ser x*y
     public int n;
-    void Start()
-    {
-    }
 
     public void increase_defense()
     {
         n++;
         defense = 1-n/30;
-
     }
 }
--- Assets/ladder.cs
+++ Assets/ladder.cs
@@ -5,31 +5,34 @@ using UnityEngine;
 public class ladder : MonoBehaviour
 {
     public float speed = 10;
+    Vector2 aux;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        void OnTriggerStay2D(Collider2D other)
+
+    }
+    void OnTriggerStay2D(Collider2D other)
+    {        
+        if (other.tag == "Player" && Input.GetKey(KeyCode.W))
         {
-            if(other.tag == "Player" && Input.GetKey(KeyCode.W))
-            {
-                    other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, speed);
-            }
+            other.GetComponent<PlatformerMotor2D>().velocity = new Vector2(0, speed);
+        }
 
-            else if(other.tag == "Player" && Input.GetKey(KeyCode.S))
-            {
-                    other.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -speed);
-            }
+        else if (other.tag == "Player" && Input.GetKey(KeyCode.S))
+        {
+            other.GetComponent<PlatformerMotor2D>().velocity = new Vector2(0, -speed);
+        }
 
-            else
-            {
-                    other.GetComponent<Rigidbody2D> ().velocity = new Vector2(0, 1);
-            }
+        else
+        {
+            aux = new Vector2(other.GetComponent<PlatformerMotor2D>().velocity.x, 0);
+            other.GetComponent<PlatformerMotor2D>().velocity = aux;
         }
     }
 
