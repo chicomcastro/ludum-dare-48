4785f424f0b644991c06b69bf362432e72d3e6a1
hirojinho <eduardo.haraguchi@gmail.com>
2021-04-26 01:01:02 -0300

    dinheiro

--- Assets/Scenes/Sonar/sis_cash.cs
+++ Assets/Scenes/Sonar/sis_cash.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class sis_cash : MonoBehaviour
 {
     public int cash = 0; // eh isso
 
+    public Text dinheiro;
+
+    public void Start()
+    {
+        dinheiro.GetComponent<Text>().text = cash.ToString();
+    }
     public void increasecash()
     {
         cash += 100;
+
+        dinheiro.GetComponent<Text>().text = cash.ToString();
     }
 }
